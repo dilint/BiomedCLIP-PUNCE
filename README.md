4532397cd4552e2e01971195848f61ed6b4d36d2
wells <15106076579@163.com>
2023-12-13 11:15:16 +0800

    update readme & add extract_features_ngc code

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # biomed-clip-puNCE
+
+
+## Train Backbone
+```bash
+# 训练backbone（2d 4张3080
+python -m torch.distributed.launch --nproc_per_node=4 simclr_ngc.py --ddp --data_dir '/home1/wsi/ngc-2023-1333' --project=$PROJECT_NAME --model_path=$OUTPUT_PATH --title=$TITLE_NAME --seed=2023 --batch_size=64 --epochs=200 --wandb
+```
+
+## Extract WSI Features
+```bash
+# 提取特征 （20min 8张2080
+GPU_NUMBERS=8
+FEAT_DIR='resnet_simclr_infonce_ngc_224_4x64_224_trainpart'
+CKP_PATH='/root/project/biomed-clip-puNCE/simclr/output-model/resnet_simclr_infonce_ngc_224_4x64_trainpart/resnet_simclr_infonce_ngc_224_4x64_trainpart_epoch130.pt'
+python -m torch.distributed.launch --nproc_per_node=$GPU_NUMBERS extract_features_ngc.py --base_model='resnet50' --output_path='result-final-ngc-features' --feat_dir=$FEAT_DIR --ckp_path=$CKP_PATH --target_patch_size 224 224 --multi_gpu
+```
+
+## MIL Train
+```bash
+# MIL整合
+# abmil  （22min 单张2080
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+TITLE_NAME=resnet3-abmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=0, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --input_dim=1024 --cv_fold=1 --title=$TITLE_NAME --model=pure --baseline=attn --seed=2023 --wandb
+
+# transmil  （38min 单张2080
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+OUTPUT_PATH=output-model
+TITLE_NAME=resnet3-transmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=0, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --input_dim=1024 --cv_fold=1 --title=$TITLE_NAME --model=pure --baseline=selfattn --seed=2023 --wandb
+
+# meanmil  （13min 单张2080
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+OUTPUT_PATH=output-model
+TITLE_NAME=resnet3-meanmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=0, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --input_dim=1024 --cv_fold=1 --title=$TITLE_NAME --model=meanmil --seed=2023 --wandb
+
+# maxmil （7min 单张2080
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+OUTPUT_PATH=output-model
+TITLE_NAME=resnet3-maxmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=0, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --input_dim=1024 --cv_fold=1 --title=$TITLE_NAME --model=maxmil --seed=2023 --wandb
+
+# MHIM(abmil)
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+OUTPUT_PATH=output-model
+TITLE_NAME=resnet3-mhim(abmil)-ngc-customsplit
+TEACHER_INIT=./output-model/output-test/biomedclip1-abmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=1, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --cv_fold=1 --input_dim=1024 --teacher_init=$TEACHER_INIT --title=$TITLE_NAME --baseline=attn --num_workers=0 --cl_alpha=0.1 --mask_ratio_h=0.01 --mask_ratio_hr=0.5 --mrh_sche --init_stu_type=fc --mask_ratio=0.5 --mask_ratio_l=0. --seed=2023 --wandb
+
+# MHIM(transmil)
+PROJECT_NAME=output-test
+DATASET_PATH=/root/project/clam/result-final-ngc-features/resnet_simclr_infonce_ngc_224_4x64_224_trainpart
+OUTPUT_PATH=output-model
+TITLE_NAME=resnet3-mhim(transmil)-ngc-customsplit
+TEACHER_INIT=./output-model/output-test/biomedclip1-transmil-ngc-customsplit
+CUDA_VISIBLE_DEVICES=0, python3 main_custome.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH --datasets=ngc --cv_fold=1 --input_dim=1024 --teacher_init=$TEACHER_INIT --mask_ratio_h=0.03 --mask_ratio_hr=0.5 --mrh_sche --title=$TITLE_NAME --mask_ratio=0. --mask_ratio_l=0.8 --cl_alpha=0.1 --mm_sche --init_stu_type=fc --attn_layer=0 --seed=2023 --wandb
+
+```
---
+++ extract-features/extract_features_ngc.py
@@ -0,0 +1,193 @@
+import torch
+import torch.nn as nn
+from math import floor
+from PIL import Image
+import open_clip
+from torchvision import transforms
+import os
+import copy
+import time
+from torch.utils.data import DataLoader, Dataset
+from models.resnet_custom import resnet50_baseline
+import argparse
+from utils.file_utils import save_hdf5
+from PIL import Image
+import h5py
+import glob
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+root_path = '/root/commonfile/wsi/ngc-2023-1333'
+# set data roots
+sub_paths = [
+	'Unannotated_KSJ/Unannotated-KSJ-TCTNGC-NILM',
+	'Unannotated_KSJ/Unannotated-KSJ-TCTNGC-POS',
+	'Unannotated_XIMEA/Unannotated-XIMEA-TCTNGC-NILM',
+	'Unannotated_XIMEA/Unannotated-XIMEA-TCTNGC-POS'
+]
+data_roots = list(map(lambda x: os.path.join(root_path, x), sub_paths)) 
+
+# patchs in one bag 
+class Whole_Slide_Patchs_Ngc(Dataset):
+    def __init__(self,
+                 wsi_path,
+                 target_patch_size,
+                 preprocess):
+        # for resnet50
+        self.preprocess = transforms.Compose([
+            transforms.Resize(target_patch_size),
+            transforms.ToTensor(),
+        ])
+        # for biomedclip
+        if preprocess != None:
+            self.preprocess = preprocess
+        self.wsi_path = wsi_path
+        self.patch_files = glob.glob(os.path.join(wsi_path, '*.jpg'))
+        
+    def __getitem__(self, idx):
+        img = Image.open(self.patch_files[idx])
+        img = self.preprocess(img)
+        return img
+    
+    def __len__(self):
+        return len(self.patch_files)
+
+    def __str__(self) -> str:
+        return f'the length of patchs in {self.wsi_path} is {self.__len__()}'
+
+def compute_w_loader(wsi_dir, 
+                      output_path, 
+                      model,
+                      preprocess_val, # for biomedclip pretrain
+                      args):
+    # set parameters
+    batch_size = args.batch_size
+    target_patch_size = args.target_patch_size
+    num_workers = args.num_workers
+    verbose = args.verbose
+    print_every = args.print_every
+    dataset = Whole_Slide_Patchs_Ngc(wsi_dir, target_patch_size, preprocess_val)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    if verbose > 0:
+        print('processing {}: total of {} batches'.format(wsi_dir,len(loader)))
+    
+    mode = 'w'
+    for i, batch in enumerate(loader):
+        with torch.no_grad():
+            if i % print_every == 0:
+                print('batch {}/{}, {} files processed'.format(i, len(loader), i * batch_size))
+            batch = batch.to(device)
+            if args.base_model == 'biomedclip':
+                features, text_features, logit_scale = model(batch) 
+            else:
+                features = model(batch)
+            features = features.cpu().numpy()
+            
+            asset_dict = {'features': features}
+            save_hdf5(output_path, asset_dict, attr_dict=None, mode=mode)
+            mode = 'a'
+    return output_path
+    
+def main():
+    # set args
+    parser = argparse.ArgumentParser(description='NGC dataset Feature Extraction')
+    parser.add_argument('--output_path', type=str, default='result-final-ngc-features')
+    parser.add_argument('--feat_dir', type=str, default='resnet-ori-test')
+    parser.add_argument('--verbose', type=int, default=0)
+    parser.add_argument('--print_every', type=int, default=20)
+    # inference options
+    parser.add_argument('--multi_gpu', action='store_true', default=False)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--num_workers', type=int, default=16)
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--world_size', type=int, default=1)
+    parser.add_argument('--target_patch_size', type=int, nargs='+', default=(224, 224))
+    # model options
+    parser.add_argument('--base_model', default='resnet50', type=str, choices=['biomedclip', 'resnet50'])
+    parser.add_argument('--model_pretrain', action='store_true', default=True)
+    parser.add_argument('--ckp_path', type=str)
+    args = parser.parse_args()
+    if args.multi_gpu:
+        args.local_rank = int(os.environ['LOCAL_RANK'])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+    # get wsi paths
+    wsi_dirs = []
+    output_path = args.output_path
+    output_path = os.path.join(output_path, args.feat_dir)
+    output_path_pt = os.path.join(output_path, 'pt')
+    output_path_h5 = os.path.join(output_path, 'h5_files')
+    for data_root in data_roots:
+        wsi_dirs.extend([os.path.join(data_root, subdir) for subdir in os.listdir(data_root)])
+
+    # get output path
+    os.makedirs(output_path_pt, exist_ok=True)
+    os.makedirs(output_path_h5, exist_ok=True)
+    dest_files = os.listdir(output_path_pt)
+    
+    
+    # load model
+    torch.cuda.set_device(args.local_rank)
+    print('loading model')
+    preprocess_val = None
+    if args.base_model == 'resnet50':
+        if args.model_pretrain:
+            model = resnet50_baseline(pretrained=True)
+        elif args.ckp_path:
+            model = resnet50_baseline(pretrained=False)
+            new_ckp = dict()
+            ckp = torch.load(args.ckp_path)
+            for key, param in ckp.items():
+                key = key.replace('module.enc.' ,'')
+                if "module.projector" in key:
+                    continue
+                new_ckp[key] = param
+            model.load_state_dict(new_ckp)
+        print('successfully load model')
+    elif args.base_model == 'biomedclip':
+        model, preprocess_train, preprocess_val = open_clip.create_model_and_transforms('hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224')
+
+    model = model.to(device)
+    
+    model.eval()
+    total = len(wsi_dirs)    
+
+    for idx in range(total):
+        if idx % args.world_size != args.local_rank:
+            continue
+        
+        wsi_dir = wsi_dirs[idx]
+        wsi_name = os.path.basename(wsi_dir)
+        print('\nprogress: {}/{}'.format(idx, total))
+        print(wsi_name)
+        
+        if wsi_name+'.pt' in dest_files:
+            print('skipped {}'.format(wsi_name))
+            continue
+        
+        output_file_path = os.path.join(output_path_h5, wsi_name+'.h5')
+        time_start = time.time()
+        compute_w_loader(wsi_dir,
+                         output_file_path,
+                         model,
+                         preprocess_val,
+                         args,
+                        )
+        time_elapesd = time.time() - time_start
+        print('\ncomputing features for {} took {} s'.format(output_path, time_elapesd))
+        
+        file = h5py.File(output_file_path, 'r')
+        
+        features = file['features'][:]
+        print('features size: ', features.shape)
+        features = torch.from_numpy(features)
+        torch.save(features, os.path.join(output_path_pt, wsi_name+'.pt'))
+
+if __name__ == '__main__':
+    time_start = time.time()
+    
+    main()
+    
+    time_end = time.time()
+    time_elapesd = time_end - time_start
+    print('\n The program took {} h {} min {} s'.format(time_elapesd//3600,
+                                                    time_elapesd%3600//60,
+                                                    time_elapesd%60))
