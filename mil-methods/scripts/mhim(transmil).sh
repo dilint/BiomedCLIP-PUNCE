38460f55aa756f26b33d039dd05a2af96dbda0e5
wells <15106076579@163.com>
2024-05-31 10:31:05 +0800

    update bug

--- mil-methods/scripts/mhim(transmil).sh
+++ mil-methods/scripts/mhim(transmil).sh
@@ -16,7 +16,7 @@ TITLE_NAME="$1-mhim(transmil)-$2-trainval"
 TEACHER_INIT=./output-model/mil-methods/$1-transmil-$2-trainval
 # TITLE_NAME="$1-mhim(transmil)-$2-trainval-$4"
 # TEACHER_INIT=./output-model/mil-methods/$1-transmil-$2-trainval-$4
-CUDA_VISIBLE_DEVICES=$GPU_ID, python3 mil.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH  --label_path=$LABEL_PATH --datasets=$2 --cv_fold=1 --input_dim=$3 --teacher_init=$TEACHER_INIT --mask_ratio_h=0.03 --mask_ratio_hr=0.5 --mrh_sche --title=$TITLE_NAME --mask_ratio=0. --mask_ratio_l=0.8 --cl_alpha=0.1 --mm_sche --init_stu_type=fc --attn_layer=0 --seed=2024 --wandb
+CUDA_VISIBLE_DEVICES=$GPU_ID, python3 mil.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --model_path=$OUTPUT_PATH  --label_path=$LABEL_PATH --datasets=$2 --cv_fold=1 --train_val --input_dim=$3 --teacher_init=$TEACHER_INIT --mask_ratio_h=0.03 --mask_ratio_hr=0.5 --mrh_sche --title=$TITLE_NAME --mask_ratio=0. --mask_ratio_l=0.8 --cl_alpha=0.1 --mm_sche --init_stu_type=fc --attn_layer=0 --seed=2024 --wandb
 
 CHECKPOINT_PATH="output-model/mil-methods/$1-mhim(transmil)-$2-trainval"
 CUDA_VISIBLE_DEVICES=$GPU_ID, python3 eval.py --ckp_path=$CHECKPOINT_PATH --label_path=$LABEL_PATH  --dataset_root=$DATASET_PATH --ckp_path=$CHECKPOINT_PATH --datasets=tct --input_dim=$3 --model=pure --baseline=selfattn --seed=2024 
