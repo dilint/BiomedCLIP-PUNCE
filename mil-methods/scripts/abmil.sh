78fe492e7cf59c8e4b559bb65061cd97117b877c
wells <15106076579@163.com>
2024-06-14 19:09:02 +0800

    update scripts

--- mil-methods/scripts/abmil.sh
+++ mil-methods/scripts/abmil.sh
@@ -4,6 +4,7 @@ echo "提取特征方式 ：$1"
 echo "数据集 ：$2"
 echo "输入维度 ：$3"
 GPU_ID=2
+SERVER=2080
 
 FEATURE_NAME=$1
 DATASET=$2
@@ -14,8 +15,8 @@ DATASET_PATH=../extract-features/result-final-$DATASET-features/$FEATURE_NAME
 LABEL_PATH=../datatools/$DATASET/labels
 OUTPUT_PATH=output-model
 PROJECT_NAME=mil-methods-info
-TITLE_NAME=$FEATURE_NAME-abmil-$DATASET-trainval
+TITLE_NAME=$FEATURE_NAME-abmil-$DATASET-trainval-$SERVER
 CUDA_VISIBLE_DEVICES=$GPU_ID, python3 mil.py --project=$PROJECT_NAME --dataset_root=$DATASET_PATH --label_path=$LABEL_PATH --model_path=$OUTPUT_PATH --datasets=$DATASET --input_dim=$NUM_DIM --cv_fold=1 --title=$TITLE_NAME --model=pure --baseline=attn --train_val --seed=2024 --wandb
 
-CHECKPOINT_PATH=$OUTPUT_PATH/$PROJECT_NAME/$FEATURE_NAME-abmil-$DATASET-trainval
+CHECKPOINT_PATH=$OUTPUT_PATH/$PROJECT_NAME/$FEATURE_NAME-abmil-$DATASET-trainval-$SERVER
 CUDA_VISIBLE_DEVICES=$GPU_ID, python3 eval.py --label_path=$LABEL_PATH  --dataset_root=$DATASET_PATH --ckp_path=$CHECKPOINT_PATH --datasets=tct --input_dim=$NUM_DIM --model=pure --baseline=attn --seed=2024
