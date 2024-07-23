7425c4a89bbdaf7a2d83c28eab24dda1fd33074a
wells <15106076579@163.com>
2024-07-23 10:18:22 +0800

    update

--- mil-methods/benchmark.py
+++ mil-methods/benchmark.py
@@ -3,9 +3,21 @@ import os
 import argparse, os
 
 def run_script(mil, args):
-    os.system(f'bash scripts/{mil}mil.sh {args.feature_name} {args.dataset} {args.num_dim}')
-    os.system(f'bash scripts/mhim\({mil}mil\).sh {args.feature_name} {args.dataset} {args.num_dim}')
-    
+    if mil == 'ab':
+        TITLE_NAME=f'{FEATURE_NAME}-abmil-{DATASET}-trainval-{SERVER}'
+        os.system(f'CUDA_VISIBLE_DEVICES=0, python3 mil.py --project={PROJECT_NAME} --dataset_root={DATASET_PATH} --label_path={LABEL_PATH} --model_path={OUTPUT_PATH} --datasets={DATASET} --input_dim={NUM_DIM} --cv_fold=1 --title={TITLE_NAME} --model=pure --baseline=attn --train_val --seed=2024 --wandb')
+
+        TITLE_NAME=f'{FEATURE_NAME}-mhim\(abmil\)-{DATASET}-trainval-{SERVER}'
+        TEACHER_INIT=f'./{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-abmil-{DATASET}-trainval-{SERVER}'
+        os.system(f'CUDA_VISIBLE_DEVICES=0, python3 mil.py --project={PROJECT_NAME} --dataset_root={DATASET_PATH} --model_path={OUTPUT_PATH} --label_path={LABEL_PATH} --datasets={DATASET} --cv_fold=1 --input_dim={NUM_DIM} --train_val --teacher_init={TEACHER_INIT} --title={TITLE_NAME} --baseline=attn --num_workers=0 --cl_alpha=0.1 --mask_ratio_h=0.01 --mask_ratio_hr=0.5 --mrh_sche --init_stu_type=fc --mask_ratio=0.5 --mask_ratio_l=0. --seed=2024 --wandb')
+    elif mil == 'trans':
+        TITLE_NAME=f'{FEATURE_NAME}-transmil-{DATASET}-trainval-{SERVER}'
+        os.system(f'CUDA_VISIBLE_DEVICES=1, python3 mil.py --project={PROJECT_NAME} --dataset_root={DATASET_PATH} --label_path={LABEL_PATH} --model_path={OUTPUT_PATH} --datasets={DATASET} --input_dim={NUM_DIM} --cv_fold=1 --title={TITLE_NAME} --model=pure --baseline=selfattn --train_val --seed=2024 --wandb')
+
+        TITLE_NAME=f'{FEATURE_NAME}-mhim\(transmil\)-{DATASET}-trainval-{SERVER}'
+        TEACHER_INIT=f'./{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-transmil-{DATASET}-trainval-{SERVER}'
+        os.system(f'CUDA_VISIBLE_DEVICES=1, python3 mil.py --project={PROJECT_NAME} --dataset_root={DATASET_PATH} --model_path={OUTPUT_PATH} --label_path={LABEL_PATH} --datasets={DATASET} --cv_fold=1 --input_dim={NUM_DIM} --train_val --teacher_init={TEACHER_INIT} --mask_ratio_h=0.03 --mask_ratio_hr=0.5 --mrh_sche --title={TITLE_NAME} --mask_ratio=0. --mask_ratio_l=0.8 --cl_alpha=0.1 --mm_sche --init_stu_type=fc --attn_layer=0 --seed=2024 --wandb')
+
 parser = argparse.ArgumentParser(description='MIL Training Script')
 parser.add_argument('feature_name', default='test', type=str, help='Feature name of extracted patch features')
 parser.add_argument('--dataset', default='gc', type=str, help='Dataset name')
@@ -13,6 +25,15 @@ parser.add_argument('--num_dim', default=512, type=int, help='number of dimensio
 
 parser.add_argument('--test_mode', action='store_true', help='Test mode')
 args = parser.parse_args()
+
+FEATURE_NAME=args.feature_name
+DATASET=args.dataset
+NUM_DIM=args.num_dim
+DATASET_PATH=f'../extract-features/result-final-{DATASET}-features/{FEATURE_NAME}'
+LABEL_PATH=f'../datatools/{DATASET}/labels'
+OUTPUT_PATH='output-model'
+PROJECT_NAME='test'
+SERVER=3080
 
 if not args.test_mode:
     # train
@@ -28,28 +49,20 @@ if not args.test_mode:
     thread2.join()
     
 # test
-FEATURE_NAME=args.feature_name
-DATASET=args.dataset
-NUM_DIM=args.num_dim
-DATASET_PATH=f'../extract-features/result-final-{DATASET}-features/{FEATURE_NAME}'
-LABEL_PATH=f'../datatools/{DATASET}/labels'
-OUTPUT_PATH='output-model'
-PROJECT_NAME='test'
-DEVICE=3080
 
 # abmil
-print('abmil')
-CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-abmil-{DATASET}-trainval-{DEVICE}'
+print('==================================\033[35mabmil\033[0m==================================')
+CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-abmil-{DATASET}-trainval-{SERVER}'
 os.system(f'CUDA_VISIBLE_DEVICES=0, python3 eval.py --label_path={LABEL_PATH} --dataset_root={DATASET_PATH} --ckp_path={CHECKPOINT_PATH} --datasets=tct --input_dim={NUM_DIM} --model=pure --baseline=attn --seed=2024')
 # mhim(abmil)
-print('mhim(abmil)')
-CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-mhim\(abmil\)-{DATASET}-trainval-{DEVICE}'
+print('==================================\033[35mmhim(abmil)\033[0m==================================')
+CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-mhim\(abmil\)-{DATASET}-trainval-{SERVER}'
 os.system(f'CUDA_VISIBLE_DEVICES=0, python3 eval.py --label_path={LABEL_PATH} --dataset_root={DATASET_PATH} --ckp_path={CHECKPOINT_PATH} --datasets=tct --input_dim={NUM_DIM} --model=pure --baseline=attn --seed=2024')
 # transmil
-print('transmil')
-CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-transmil-{DATASET}-trainval-{DEVICE}'
+print('==================================\033[35mtransmil\033[0m==================================')
+CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-transmil-{DATASET}-trainval-{SERVER}'
 os.system(f'CUDA_VISIBLE_DEVICES=0, python3 eval.py --label_path={LABEL_PATH} --dataset_root={DATASET_PATH} --ckp_path={CHECKPOINT_PATH} --datasets=tct --input_dim={NUM_DIM} --model=pure --baseline=selfattn --seed=2024')
 # mhim(transmil)
-print('mhim(transmil)')
-CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-mhim\(transmil\)-{DATASET}-trainval-{DEVICE}'
+print('==================================\033[35mmhim(transmil)\033[0m==================================')
+CHECKPOINT_PATH=f'{OUTPUT_PATH}/{PROJECT_NAME}/{FEATURE_NAME}-mhim\(transmil\)-{DATASET}-trainval-{SERVER}'
 os.system(f'CUDA_VISIBLE_DEVICES=0, python3 eval.py --label_path={LABEL_PATH} --dataset_root={DATASET_PATH} --ckp_path={CHECKPOINT_PATH} --datasets=tct --input_dim={NUM_DIM} --model=pure --baseline=selfattn --seed=2024')
