a40e66d1f9cbede695c0f45d501b057a855a088f
wells <15106076579@163.com>
2024-12-16 14:35:12 +0800

    update

--- tools/extract-features.sh
+++ tools/extract-features.sh
@@ -9,4 +9,4 @@ DATASET='gc-2000' # 'ngc', 'ubc', 'gc', 'fnac', 'gc-2000'
 GPU_NUMBERS=4
 WSI_ROOT='/data/wsi/TCTGC50k/TCTGC50k-volume1' 
 OUTPUT_PATH='/data/wsi/TCTGC50k-features'
-python -m torch.distributed.launch --nproc_per_node=$GPU_NUMBERS extract_features_tct.py --base_model=${BASE_MODEL} --dataset=${DATASET} --output_path=$OUTPUT_PATH --feat_dir=$FEAT_DIR --wsi_root=$WSI_ROOT --ckp_path=$CKP_PATH --target_patch_size 224 224 --multi_gpu --batch_size=32 --num_workers=64
+python -m torch.distributed.launch --nproc_per_node=$GPU_NUMBERS extract_features_tct.py --base_model=${BASE_MODEL} --dataset=${DATASET} --output_path=$OUTPUT_PATH --feat_dir=$FEAT_DIR --wsi_root=$WSI_ROOT --ckp_path=$CKP_PATH --target_patch_size 224 224 --multi_gpu --batch_size=16 --num_workers=32
